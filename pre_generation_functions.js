0055feb4f588b428a853f5cdd9ce80507d238eb8
Madis <madisjanno@gmail.com>
2020-05-08 00:17:16 +0300

    mousewheel to change terrain height

--- pre_generation_functions.js
+++ pre_generation_functions.js
@@ -1,11 +1,22 @@
 // Creates colored sphere with a "category" as parent
-function createDebugSphere(position, color, category) {
+function createDebugSphere(position, color, category, internalList) {
     var material = new THREE.MeshBasicMaterial( { color: color } );
     var mesh = new THREE.Mesh( debugGeometry, material );
     mesh.position.copy( position );
-    debugCategories[category].add(mesh)
+    debugCategories[category].add(mesh);
+    internalList.push(mesh);
     return mesh;
 }
+
+function pregenerateHexGeometry(hexes, regenerate=false) {
+    var newEdges = generateHexEdges(hexes, regenerate);
+    generateHexTriplets(hexes.filter(h => h.active), regenerate);
+    finishEdges(newEdges);
+}
+
+// TRIPLET FUNCTIONS
+//
+//
 
 function getTriplet(a,b,c, generate=false) {
     if (a.isHex)
@@ -34,30 +45,269 @@ function getTriplet(a,b,c, generate=false) {
     }
     if (generate) {
         var n = generateTriplet(hexArray[a],hexArray[b],hexArray[c]);
-        triplets[a].push(n)
+        triplets[a].push(n);
+        generate.add(n);
         return n;
     } else
         throw new Error("Triplet " + [a,b,c] + " not found.")
 }
 
-function generateTriplets() {
-    for (var i = 0; i < latestID; i++) {
-        triplets.push([])
-    }
+function generateAllTriplets() {    
+    generateHexTriplets(hexArray.filter(h => h.active));
+}
 
-    for (hex of hexArray) {
-        if (!hex.active)
-            continue;
+function generateHexTriplets(hexes, regenerate = false) {
+    regennedSet = new Set();
+    
+    for (hex of hexes) {
+        while (triplets.length < hex.id+1)
+            triplets.push([]);
+        
+        var j = 0;
+        for (var i = 0; i < hex.neighbours.length; i++) {
+            j = (i+1) % hex.neighbours.length;
 
-        generateHexTriplets(hex);
+            tri = getTriplet(hex, hex.neighbours[i], hex.neighbours[j], generate=regennedSet);
+            
+            if (regenerate && !regennedSet.has(tri)) {
+                generateTriplet(hex, hex.neighbours[i], hex.neighbours[j], regenerate=tri);
+                regennedSet.add(tri);
+            }
+        }
     }
 }
 
-function generateHexTriplets(hex) {
-    var j = 0;
-    for (var i = 0; i < hex.neighbours.length; i++) {
-        j = (i+1) % hex.neighbours.length;
+function generateTriplet(a,b,c, regenerate=false) {
+    var triplet = {a:a.id,b:b.id,c:c.id,  spheres:[]};
 
-        tri = getTriplet(hex, hex.neighbours[i], hex.neighbours[j], generate=true)
+    if (regenerate)
+        triplet = regenerate;
+    
+    for (sphere of triplet.spheres) {
+        sphere.material.dispose();
+        sphere.parent.remove(sphere);
+    }
+    triplet.spheres = [];
+
+    triplet.position =  a.centre.clone().add(b.centre).add(c.centre).divideScalar(3);
+    
+    triplet.triple_cliff = false;
+
+    var ab = getEdge(a,b);
+    var bc = getEdge(c,b);
+    var ac = getEdge(c,a);
+
+    ab.triplets.push(triplet);
+    bc.triplets.push(triplet);
+    ac.triplets.push(triplet);
+
+    var vertices = {}
+
+    if (ab.cliff & bc.cliff & ac.cliff) {
+        triplet.a_pos = triplet.position.clone().setComponent(1, a.centre.y);
+        triplet.b_pos = triplet.position.clone().setComponent(1, b.centre.y);
+        triplet.c_pos = triplet.position.clone().setComponent(1, c.centre.y);
+
+        var upper = Math.max(a.centre.y+b.centre.y, b.centre.y+c.centre.y, a.centre.y+c.centre.y)/2;
+        var lower = Math.min(a.centre.y+b.centre.y, b.centre.y+c.centre.y, a.centre.y+c.centre.y)/2;
+
+        triplet.upper_mid = triplet.position.clone().setComponent(1, upper);
+        triplet.lower_mid = triplet.position.clone().setComponent(1, lower);
+
+        triplet.mid = triplet.lower_mid;
+
+        vertices[a.id] = triplet.a_pos;
+        vertices[b.id] = triplet.b_pos;
+        vertices[c.id] = triplet.c_pos;
+
+        triplet.triple_cliff = true;
+
+        triplet.vertArr = [triplet.a_pos, triplet.b_pos, triplet.c_pos, triplet.upper_mid, triplet.lower_mid];
+
+        createDebugSphere(triplet.a_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.b_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.c_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        
+        createDebugSphere(triplet.upper_mid, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.lower_mid, 0xff0000, "cliffTriplets", triplet.spheres);
+    } else if (ab.cliff & bc.cliff) {
+        triplet.a_pos = triplet.position.clone().setComponent(1, (a.centre.y+c.centre.y)/2 );
+        triplet.b_pos = triplet.position.clone().setComponent(1, b.centre.y);
+        triplet.c_pos = triplet.a_pos;
+
+        triplet.mid = triplet.a_pos.clone().add(triplet.b_pos).divideScalar(2);
+
+        vertices[a.id] = triplet.a_pos;
+        vertices[b.id] = triplet.b_pos;
+        vertices[c.id] = triplet.c_pos;
+
+        triplet.vertArr = [triplet.a_pos, triplet.b_pos, triplet.mid];
+
+        createDebugSphere(triplet.a_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.b_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.mid, 0xff0000, "cliffTriplets", triplet.spheres);
+    } else if (ab.cliff & ac.cliff) {
+        triplet.a_pos = triplet.position.clone().setComponent(1, a.centre.y);
+        triplet.b_pos = triplet.position.clone().setComponent(1, (b.centre.y+c.centre.y)/2 );
+        triplet.c_pos = triplet.b_pos;
+
+        triplet.mid = triplet.a_pos.clone().add(triplet.b_pos).divideScalar(2);
+
+        vertices[a.id] = triplet.a_pos;
+        vertices[b.id] = triplet.b_pos;
+        vertices[c.id] = triplet.c_pos;
+
+        triplet.vertArr = [triplet.a_pos, triplet.b_pos, triplet.mid];
+
+        createDebugSphere(triplet.a_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.b_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.mid, 0xff0000, "cliffTriplets", triplet.spheres);
+    } else if (bc.cliff & ac.cliff) {
+        triplet.a_pos = triplet.position.clone().setComponent(1, (a.centre.y+b.centre.y)/2 );
+        triplet.b_pos = triplet.a_pos;
+        triplet.c_pos = triplet.position.clone().setComponent(1, c.centre.y);
+
+        triplet.mid = triplet.a_pos.clone().add(triplet.c_pos).divideScalar(2);
+
+        vertices[a.id] = triplet.a_pos;
+        vertices[b.id] = triplet.b_pos;
+        vertices[c.id] = triplet.c_pos;
+
+        triplet.vertArr = [triplet.a_pos, triplet.c_pos, triplet.mid];
+
+        createDebugSphere(triplet.a_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.c_pos, 0xff0000, "cliffTriplets", triplet.spheres);
+        createDebugSphere(triplet.mid, 0xff0000, "cliffTriplets", triplet.spheres);
+    } else {
+        triplet.a_pos = triplet.position;
+        triplet.b_pos = triplet.position;
+        triplet.c_pos = triplet.position;
+        triplet.mid = triplet.position;
+
+        vertices[a.id] = triplet.a_pos;
+        vertices[b.id] = triplet.b_pos;
+        vertices[c.id] = triplet.c_pos;
+
+        triplet.vertArr = [triplet.position];
+
+        createDebugSphere(triplet.position, 0xffff00, "regularTriplets", triplet.spheres);
+    }
+
+    triplet.vertices = vertices;
+
+    return triplet;
+}
+
+// EDGE FUNCTIONS
+//
+//
+
+function getEdge(a,b, generate = false) {
+    if (a.isHex)
+        a = a.id;
+    if (b.isHex)
+        b = b.id;
+
+    if (a > b) {
+        a = b + (b=a, 0)
+    }
+
+    for (p of edges[a]) {
+        if (p.b == b) {
+            return p;
+        }
+    }
+    if (generate) {
+        var e = generateEdge(hexArray[a],hexArray[b]);
+        edges[a].push(e);
+        generate.add(e);
+        return e;
+    } else
+        throw new Error("Edge " + [a,b] + " not found.")
+}
+
+function generateAllEdges() {
+    return generateHexEdges(hexArray);
+}
+
+function generateHexEdges(hexes, regenerate = false) {
+    var regennedSet = new Set();
+    
+    for (hex of hexes) {
+        while (edges.length < hex.id+1)
+            edges.push([]);
+        
+        for (n of hex.neighbours) {
+            var e = getEdge(hex, n, generate=regennedSet);
+            
+            if (regenerate && !regennedSet.has(e)) {
+                generateEdge(hex, n, regenerate=e);
+                regennedSet.add(e);
+            }
+        }
+    }
+    
+    return regennedSet;
+}
+
+function generateEdge(a,b, regenerate=false) {
+    var edge = {a:a.id, b:b.id, triplets:[], spheres:[]};
+    
+    if (regenerate)
+        edge = regenerate;
+    
+    for (sphere of edge.spheres) {
+        sphere.material.dispose();
+        sphere.parent.remove(sphere);
+    }
+    edge.spheres = [];
+
+    for (d of a.neighboursData) {
+        if (d.hex == b) {
+            edge.cliff = d.cliff;
+            break;
+        }
+    }
+
+    edgeArray.push(edge);
+
+    return edge;
+}
+
+function finishEdges(edges) {
+    for (edge of edges) {
+        if (!hexArray[edge.a].active && !hexArray[edge.b].active) {
+            continue;
+        }
+        edge.active = true;
+
+        var a = hexArray[edge.a];
+        var b = hexArray[edge.b];
+
+        edge.position = edge.triplets[0].vertices[a.id].clone().add(edge.triplets[1].vertices[a.id]).divideScalar(2);
+
+        edge.vertices = {}
+        edge.vertArr = [];
+        if (edge.cliff) {
+            edge.vertices[a.id] = edge.position.clone().setComponent(1, a.centre.y );
+            edge.vertices[b.id] = edge.position.clone().setComponent(1, b.centre.y );
+
+            edge.mid = edge.position.clone().setComponent(1, (a.centre.y+b.centre.y)/2 );
+
+            edge.vertArr.push(edge.vertices[a.id], edge.vertices[b.id], edge.mid);
+
+            createDebugSphere(edge.vertices[a.id], 0x0000ff, "cliffEdges", edge.spheres);
+            createDebugSphere(edge.vertices[b.id], 0x0000ff, "cliffEdges", edge.spheres);
+            createDebugSphere(edge.mid, 0x0000ff, "cliffEdges", edge.spheres);
+        } else {
+            edge.vertices[a.id] = edge.position;
+            edge.vertices[b.id] = edge.position;
+            edge.mid = edge.position;
+
+            edge.vertArr.push(edge.position);
+
+            createDebugSphere(edge.position, 0x00ffff, "regularEdges", edge.spheres);
+        }
+
     }
 }
